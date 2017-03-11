0a69a4a382f6272d71de0ce5ff688ef778f8e343
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-03-11 11:57:35 -0800

    Initialize App.js component and dependencies

--- FOODr3k/src/components/App.js
+++ FOODr3k/src/components/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
-
-import { Screen, Spinner, Examples } from '@shoutem/ui';
-import { stringify as queryString } from 'query-string';
-
-const OPENTABLE_ENDPOINT = 'http://opentable.herokuapp.com/api/restaurants?zip=94107';
+import axios from 'axios';
+import _ from 'lodash';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import MapView, { MAP_TYPES } from 'react-native-maps';
+const { width, height } = Dimensions.get('window');
 
 class App extends Component {
-   state = {
-        mapRegion: null,
-        gpsAccuracy: null,
-        recommendations: [],
-        lookingFor: null,
-        headerLocation: null,
-        otApiCall: null
-    }
-    watchID = null
-
-    componentWillMount() {
-        this.watchID = navigator.geolocation.watchPosition((position) => {
-            let region = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                latitudeDelta: 0.00922*1.5,
-                longitudeDelta: 0.00421*1.5
-            }
-
-            this.onRegionChange(region, position.coords.accuracy);
-        });
-    }
-
-    componentWillUnmount() {
-        navigator.geolocation.clearWatch(this.watchID);
-    }
 
 }
