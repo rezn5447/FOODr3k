6c5f758d4a05b6e330aa1e9f674f823b6ddd717f
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-03-11 11:55:26 -0800

    Reinitialize ios version of app and clean folder paths

--- FOODr3k/index.ios.js
+++ FOODr3k/index.ios.js
@@ -1,26 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import { AppRegistry } from 'react-native';
+import App from './src/components/App';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import App from './components/app';
-import { Examples } from '@shoutem/ui';
-
-export default class FOODr3k extends Component {
-  render() {
-    return (
-      <App />
-    );
-  }
-}
-
-AppRegistry.registerComponent('FOODr3k', () => FOODr3k);
+AppRegistry.registerComponent('FOODr3k', () => App);
