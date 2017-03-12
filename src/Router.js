f803fcab03c00812f0273672f3f036c5984f76c7
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-11 18:36:12 -0800

    Add detail screen

--- src/Router.js
+++ src/Router.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Image, View } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 import GoogleMap from './components/GoogleMap';
+import Detail from './components/Detail';
 
 const RouterComponent = () => {
 	return (
 		<Router sceneStyle={{ paddingTop: 65, flexGrow: 1 }} >
-			<Scene key="auth">
+			<Scene key="map">
 				<Scene 
-				key="GoogleMap" 
+				key="googlemap" 
 				component={GoogleMap} 
 				title="Fooder3k"
+				/>
+				<Scene 
+				key="detail" 
+				component={Detail} 
+				title="Details"
 				/>
 			</Scene>
 		</Router>
